430ad6b2c6331d767e73e12b7315a833df2613c4
Cthompsonlbi <chad.thompson@Qorvo.com>
2021-11-03 17:01:39 -0400

    Continued my py practice

--- Practice_temp_python.py
+++ Practice_temp_python.py
@@ -1,6 +1,14 @@
-temperature = int(input("What is the temperature outside?"))
+# temperature = int(input("What is the temperature outside?"))
 
-if temperature > 80:
-    print("Turn on the AC.")
-else:
-    print("Open the windows.")
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+    
+for num in range(5):
+    print(num)
+    
---
+++ loops.py
@@ -0,0 +1,11 @@
+#Loops Practice
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 2
+
